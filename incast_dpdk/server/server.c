a9e41dfeed37fc37374225e341024c7a155c1d0b
mrrr61 <mrrr61@outlook.com>
2025-05-02 17:06:15 +0800

    Fix output_dbg_data file name.

--- incast_dpdk/server/server.c
+++ incast_dpdk/server/server.c
@@ -133,9 +133,9 @@ static int32_t nc_backend_loop(__attribute__((unused)) void *arg) {
     uint64_t rx_array[150] = {};
     uint32_t rx_idx = 0;
     open_output_file();
-    output_dbg_file = fopen("output_dbg_data.txt", "w");
+    output_dbg_file = fopen("output_dbg_data.bin", "w");
     if (output_dbg_file == NULL) {
-        perror("Failed to open file output_dbg_data.txt");
+        perror("Failed to open file output_dbg_data.bin");
         exit(EXIT_FAILURE);
     }
     while (1) {
